9ab6af0ba2c733a5b67ed53b7fea99fec9b81c64
Gl-Azul-MX <glazulmx@gmail.com>
2024-10-23 09:09:05 -0700

    Update main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -1,22 +1,22 @@
 import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
-let img = await (await fetch(`https://i.ibb.co/3N4StyG/file.jpg`)).buffer()
+let img = await (await fetch(`https://i.postimg.cc/RZSx2W3M/IMG-20241022-WA0152.jpg`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
-let txt = `*Hola!, te invito a unirte a los grupos oficiales de del Bot para convivir con la comunidad :D*
+let txt = `*𝐇𝐎𝐋𝐀, 𝐓𝐄 𝐈𝐍𝐕𝐈𝐓𝐎 𝐀 𝐐𝐔𝐄 𝐓𝐄 𝐔𝐍𝐀𝐒 𝐀𝐋 𝐆𝐑𝐔𝐏𝐎 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 𝐃𝐄 𝐄𝐋 𝐓𝐄𝐀𝐌 𝐍𝐀𝐊𝐀𝐍𝐎*
 
-1- https://chat.whatsapp.com/IozDcUnkCoVDBQBDNYF6GB
+1- https://chat.whatsapp.com/LlWSHEMR67ZE8t8mfLm8fu
 *✰* ${group}
 
 *─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ*
 
-➠ Enlace anulado? entre aquí! 
+➼ 𝐒𝐈 𝐄𝐋 𝐄𝐍𝐋𝐀𝐂𝐄 𝐍𝐎 𝐅𝐔𝐍𝐂𝐈𝐎𝐍𝐀 𝐄𝐍𝐓𝐑𝐀 𝐀𝐋 𝐄𝐍𝐋𝐀𝐂𝐄 𝐃𝐄 𝐀𝐁𝐀𝐉𝐎! 
 
 Canal :
 *✰* ${canal}
 
-> 🚩 ${textbot}`
+> 💥 ${textbot}`
 await conn.sendFile(m.chat, img, "Thumbnail.jpg", txt, m, null, rcanal)
 }
 handler.help = ['grupos']
